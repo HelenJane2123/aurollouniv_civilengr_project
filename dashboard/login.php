3ee30a7da9cfbf4bd7ac66a5340b3627969862ef
DESKTOP-NJITH4B\HOME <manalohelenjane@gmail.com>
2022-10-18 18:47:19 +0800

    push additional updates.

--- dashboard/login.php
+++ dashboard/login.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Aurollo University - Civil Engineering Reviewer - Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -60,7 +60,7 @@
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Back to Homepage</a>
+                                        <a class="small" href="../index.php">Back to Homepage</a>
                                     </div>
                                 </div>
                             </div>
