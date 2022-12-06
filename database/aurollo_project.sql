3021ad4eeade8cff50b815e2261b814765ff910e
DESKTOP-NJITH4B\HOME <manalohelenjane@gmail.com>
2022-12-05 20:49:41 -0500

    push updates.

--- database/aurollo_project.sql
+++ database/aurollo_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2022 at 05:04 PM
+-- Generation Time: Dec 06, 2022 at 02:48 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -40,16 +40,6 @@ CREATE TABLE `exam` (
   `date_modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `exam`
---
-
-INSERT INTO `exam` (`exam_id`, `exam_category_id`, `program_id`, `exam_description`, `duration`, `total_questions`, `member_id`, `date_created`, `exam_status`, `date_modified`) VALUES
-(10, 2, 32, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 2, 3, 'M-1705', '2022-11-30', 'Added', '0000-00-00 00:00:00'),
-(11, 2, 33, '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>', 3, 2, 'M-1705', '2022-11-30', 'Added', '0000-00-00 00:00:00'),
-(12, 2, 34, '<p>fsdfsdfsadfsadfsa</p>', 3, 2, 'M-1705', '2022-12-01', 'Added', '0000-00-00 00:00:00'),
-(13, 1, 35, '', 0, 0, 'M-1705', '2022-12-01', 'Added', '0000-00-00 00:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -80,19 +70,6 @@ CREATE TABLE `exam_details` (
   `date_modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `exam_details`
---
-
-INSERT INTO `exam_details` (`exam_details_id`, `exam_id`, `question_no`, `question`, `date_created`, `date_modified`) VALUES
-(104, 10, 1, 'Question 1 sdfsafsdf', '2022-11-30 03:56:57', '0000-00-00 00:00:00'),
-(105, 10, 2, 'Question 2 sdasdasd', '2022-11-30 03:56:58', '0000-00-00 00:00:00'),
-(106, 10, 3, 'Question 3 sadasdfas', '2022-11-30 03:56:58', '0000-00-00 00:00:00'),
-(107, 11, 1, 'Question 1 sdfsdfsd', '2022-11-30 03:59:22', '0000-00-00 00:00:00'),
-(108, 11, 2, 'Question 2 sadasfdasedfs', '2022-11-30 03:59:23', '0000-00-00 00:00:00'),
-(109, 12, 1, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever', '2022-12-01 04:42:13', '0000-00-00 00:00:00'),
-(110, 12, 2, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever sdfsdfsadfsadfsa', '2022-12-01 04:42:14', '0000-00-00 00:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -107,51 +84,6 @@ CREATE TABLE `exam_details_answer` (
   `correct_answer` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `exam_details_answer`
---
-
-INSERT INTO `exam_details_answer` (`exam_details_ans_id`, `question_no`, `exam_details_id`, `answers`, `correct_answer`) VALUES
-(132, 3, 101, 'ffsdfsdfsdf', '0'),
-(133, 3, 101, 'dfsdfsdf', '1'),
-(134, 3, 101, 'sdfsdfsdfsd', '0'),
-(135, 4, 102, 'hfghfg', '1'),
-(136, 4, 102, 'ghfghfdg', '0'),
-(137, 4, 102, 'hfgh', '0'),
-(138, 4, 102, 'fghfghfdghfd', '0'),
-(139, 5, 103, 'fgfdghfgh', '1'),
-(140, 5, 103, 'gfhfg', '0'),
-(141, 5, 103, 'hfghfg', '0'),
-(142, 5, 103, 'hfghfgd', '0'),
-(143, 1, 104, 'Question 101', '1'),
-(144, 1, 104, 'Question 102', '0'),
-(145, 1, 104, 'Question 103', '0'),
-(146, 1, 104, 'Question 104', '0'),
-(147, 2, 105, 'Question 201', '0'),
-(148, 2, 105, 'Question 202', '1'),
-(149, 2, 105, 'Question 203', '0'),
-(150, 2, 105, 'Question 204', '0'),
-(151, 3, 106, 'Question 301', '0'),
-(152, 3, 106, 'Question 302', '0'),
-(153, 3, 106, 'Question 303', '1'),
-(154, 3, 106, 'Question 304', '0'),
-(155, 1, 107, 'dfsdfsd', '0'),
-(156, 1, 107, 'fgsadfg', '1'),
-(157, 1, 107, 'fgdfgdfsgds', '0'),
-(158, 1, 107, 'fdgdfgdfgd', '0'),
-(159, 2, 108, 'gdfg', '0'),
-(160, 2, 108, 'dfgdg', '0'),
-(161, 2, 108, 'dfgdsgdfsg', '0'),
-(162, 2, 108, 'fgdfsgdsfg', '1'),
-(163, 1, 109, 'fsdfsadf', '1'),
-(164, 1, 109, 'sdfsad', '0'),
-(165, 1, 109, 'fsadfsad', '0'),
-(166, 1, 109, 'fasdfsadfsad', '0'),
-(167, 2, 110, 'fsadfs', '0'),
-(168, 2, 110, 'adfsdf', '1'),
-(169, 2, 110, 'sdfsd', '0'),
-(170, 2, 110, 'fsdfsdfsdaf', '0');
-
 -- --------------------------------------------------------
 
 --
@@ -165,13 +97,6 @@ CREATE TABLE `exam_essay` (
   `date_created` datetime NOT NULL,
   `date_modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `exam_essay`
---
-
-INSERT INTO `exam_essay` (`exam_essay_id`, `exam_id`, `essay`, `date_created`, `date_modified`) VALUES
-(4, 13, '<ol>\r\n<li class=\"p1\"><span class=\"s1\">Do you agree or disagree with the following statement? Parents are the best teachers. Use specific reasons and examples to support your answer.&nbsp;</span></li>\r\n</ol>', '2022-12-01 12:50:12', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -190,16 +115,6 @@ CREATE TABLE `programs` (
   `date_modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `programs`
---
-
-INSERT INTO `programs` (`program_id`, `member_id`, `program_name`, `short_desc`, `with_exam`, `upload_image`, `date_created`, `date_modified`) VALUES
-(32, 'M-1705', 'Test Program 6', '<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.</p>', 1, '171142.jpg', '2022-11-30 03:53:02', '0000-00-00 00:00:00'),
-(33, 'M-1705', 'Test Program 1', '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>', 1, '632900.jpg', '2022-11-30 03:58:27', '0000-00-00 00:00:00'),
-(34, 'M-1705', 'Test Program 4', '<p>fsdfsdfsdfsdfsdfs</p>', 1, 'Spring-Images-Desktop-Wallpaper.jpg', '2022-12-01 04:41:09', '0000-00-00 00:00:00'),
-(35, 'M-1705', 'Test Program 2', '<p>dfsdfsdfsdfsdfsdfsdfsdf</p>', 1, '20473183.png', '2022-12-01 12:48:50', '0000-00-00 00:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -211,16 +126,6 @@ CREATE TABLE `program_additioonal_info` (
   `program_id` int(50) NOT NULL,
   `program_uploaded_files` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `program_additioonal_info`
---
-
-INSERT INTO `program_additioonal_info` (`program_info_id`, `program_id`, `program_uploaded_files`) VALUES
-(27, 32, '05_Handout_19-Queues...pdf'),
-(28, 33, '04_Handout_191-Stacks.pdf'),
-(29, 34, 'medical.pdf'),
-(30, 35, '06_Handout_16-Trees.pdf');
 
 -- --------------------------------------------------------
 
@@ -243,17 +148,6 @@ CREATE TABLE `students` (
   `date_modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`student_id`, `account_id`, `member_id`, `student_member_id`, `program_id`, `exam_attempt`, `stud_exam_status`, `exam_score`, `score_status`, `prof_comment_if_essay`, `unenroll_student`, `date_modified`) VALUES
-(40, 27, 'M-1705', 'M-5882', 32, 3, 2, 67, 'Satisfactory', '', 0, '2022-12-05 04:11:18'),
-(41, 27, 'M-1705', 'M-5882', 34, 3, 2, 50, 'Failed', '', 0, '2022-12-05 04:13:43'),
-(42, 31, 'M-1705', 'M-486', 34, 3, 2, 100, 'Outstanding', '', 0, '2022-12-05 04:20:09'),
-(43, 28, 'M-1705', 'M-3744', 35, 0, 0, 0, '', '', 0, '2022-12-05 04:21:05'),
-(44, 31, 'M-1705', 'M-486', 35, 0, 0, 0, '', '', 0, '2022-12-05 05:02:31');
-
 -- --------------------------------------------------------
 
 --
@@ -270,33 +164,6 @@ CREATE TABLE `student_answers` (
   `exam_taken` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `student_answers`
---
-
-INSERT INTO `student_answers` (`student_answer_id`, `exam_id`, `student_id`, `exam_details_ans_id`, `answer`, `status`, `exam_taken`) VALUES
-(151, 10, 40, 143, 1, 'old', '2022-12-05 03:39:10'),
-(152, 10, 40, 147, 1, 'old', '2022-12-05 03:39:11'),
-(153, 10, 40, 151, 1, 'old', '2022-12-05 03:39:12'),
-(170, 10, 40, 143, 1, 'old', '2022-12-05 04:10:23'),
-(171, 10, 40, 147, 0, 'old', '2022-12-05 04:10:31'),
-(172, 10, 40, 151, 0, 'old', '2022-12-05 04:10:39'),
-(173, 10, 40, 143, 1, 'new', '2022-12-05 04:10:56'),
-(174, 10, 40, 147, 1, 'new', '2022-12-05 04:11:04'),
-(175, 10, 40, 151, 0, 'new', '2022-12-05 04:11:10'),
-(176, 12, 41, 163, 0, 'old', '2022-12-05 04:12:53'),
-(177, 12, 41, 167, 0, 'old', '2022-12-05 04:12:54'),
-(178, 12, 41, 163, 1, 'old', '2022-12-05 04:13:15'),
-(179, 12, 41, 167, 0, 'old', '2022-12-05 04:13:22'),
-(180, 12, 41, 163, 0, 'new', '2022-12-05 04:13:38'),
-(181, 12, 41, 167, 1, 'new', '2022-12-05 04:13:40'),
-(182, 12, 42, 163, 1, 'old', '2022-12-05 04:19:33'),
-(183, 12, 42, 167, 0, 'old', '2022-12-05 04:19:35'),
-(184, 12, 42, 163, 0, 'old', '2022-12-05 04:19:53'),
-(185, 12, 42, 167, 1, 'old', '2022-12-05 04:19:56'),
-(186, 12, 42, 163, 1, 'new', '2022-12-05 04:20:04'),
-(187, 12, 42, 167, 1, 'new', '2022-12-05 04:20:06');
-
 -- --------------------------------------------------------
 
 --
@@ -310,13 +177,6 @@ CREATE TABLE `student_essay_answer` (
   `student_answer` longtext NOT NULL,
   `exam_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `student_essay_answer`
---
-
-INSERT INTO `student_essay_answer` (`essay_answer_id`, `exam_essay_id`, `student_id`, `student_answer`, `exam_id`) VALUES
-(6, 4, 33, '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 13);
 
 -- --------------------------------------------------------
 
@@ -343,17 +203,6 @@ CREATE TABLE `user_account` (
   `date_updated` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `user_account`
---
-
-INSERT INTO `user_account` (`id`, `member_id`, `email_address`, `firstname`, `last_name`, `phone_number`, `age`, `birthday`, `religion`, `blood_type`, `upload_image`, `gender`, `user_type`, `password`, `date_created`, `date_updated`) VALUES
-(27, 'M-5882', 'manalohelenjane@gmail.com', 'Helen Jane', 'Manalo', 41465465, 30, '1991-07-23', 'test', 'A', '', 'Female', 'Student', 'password', '2022-10-24 10:53:40am', '2022-11-26 02:20:03pm'),
-(28, 'M-3744', 'johndoe@gmail.com', 'John', 'Doe', 41465465, 0, '', '', '0', '0', '', 'Student', 'password', '2022-10-24 10:54:35am', ''),
-(29, 'M-4299', 'manalohelenjane1@gmail.com', 'Helen Jane 1', 'Manalo', 41465465, 0, '', '', '0', '0', '', 'Student', 'password', '2022-10-24 10:55:50am', ''),
-(30, 'M-1705', 'johndoemaster@gmail.com', 'John Master', 'Doe 1', 41465465, 30, '2022-11-11', 'test', 'A+', '', 'Male', 'Professor', 'password', '2022-10-24 01:20:28pm', '2022-11-30 01:37:14pm'),
-(31, 'M-486', 'hecer01@gmail.com', 'Hecer', 'Test ', 41465465, 20, '2002-11-30', 'Test', 'A', '', 'Male', 'Student', 'password', '2022-11-30 01:22:46pm', '2022-11-30 01:26:03pm');
-
 -- --------------------------------------------------------
 
 --
@@ -369,15 +218,6 @@ CREATE TABLE `user_additional_information` (
   `section` varchar(100) NOT NULL,
   `class` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user_additional_information`
---
-
-INSERT INTO `user_additional_information` (`user_account_id`, `member_id`, `academic_year`, `teaching_class`, `course`, `section`, `class`) VALUES
-(1, 'M-1705', 2022, 'test', '', 'test', ''),
-(2, 'M-5882', 2022, '', 'Electical Engineering', 'Test', '1B'),
-(3, 'M-486', 2022, '', 'Information Technology', 'Testing', '1B');
 
 --
 -- Indexes for dumped tables
